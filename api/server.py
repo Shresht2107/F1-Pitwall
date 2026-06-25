b00f95cd633ffe6a947a21952591c4a78c600d37
Shresht-Ashish <shresht.ashish18@gmail.com>
2026-06-25 17:55:07 +0530

    Update CORS to allow f1-pitwall-beta.vercel.app

--- api/server.py
+++ api/server.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://pitwall.vercel.app",
+        "https://f1-pitwall-beta.vercel.app",
         "https://*.vercel.app",
     ],
     allow_credentials=True,
